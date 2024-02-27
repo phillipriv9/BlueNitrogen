3685ae9b8a7a2e65595767a34abd8f3c4175b20b
Phillipriv9 <priver02@villanova.edu>
2024-02-27 09:07:25 -0500

    Asanopoulos Data and Script

---
+++ scripts/data_refinement/Asanpoulos.R
@@ -0,0 +1,30 @@
+#Import data from Asanpoulos et al. 2021
+
+library(magrittr)
+library(dplyr)
+library(tidyverse)
+
+input_data01 <- read.csv('data/raw_data/asanopoulos.csv')
+
+#remove row one
+
+input_data01 <- input_data01[-1,]
+
+#### Add informational
+source_name <- "Asanopoulos"
+author_initials <- "ca"
+
+#### export ####
+
+export_data01 <- input_data01 %>% 
+  dplyr::select(study_id, site_id, core_id, Habitat_type, Year_collected, 
+                Latitude, Longitude, U_depth_m, L_depth_m, OC_perc, N_perc, BD_reported_g_cm3,DOI)
+
+## export
+
+path_out = 'data/refined/'
+
+export_file <- paste(path_out, source_name, ".csv", sep = '') 
+export_df <- export_data01
+
+write.csv(export_df, export_file)
